5500ee9328ccbdeb83e32938917c89951ebafb06
Ryan Westlund <rlwestlund@gmail.com>
2018-05-23 14:02:52 +0000

    Added a handle for the SIGINFO signal

--- main.go
+++ main.go
@@ -9,16 +9,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"golang.org/x/sys/unix"
 	"log"
 	"os"
 	"os/signal"
-
-	"golang.org/x/sys/unix"
+	"text/tabwriter"
 )
 
 // The location of the configuration file to read. Compile with .e.g.
 // -ldflags="-X main.localbase=/usr/local" if the config file will not be under
 // /etc/.
+
+func printYN(b bool) string {
+	if b {
+		return "Yes"
+	} else {
+		return "No"
+	}
+}
 
 var localbase = ""
 
@@ -28,6 +37,8 @@ func main() {
 	g.RunningChan = make(chan launchStatus)
 	g.DoneChan = make(chan launchStatus)
 	g.Procs = make(map[string]*process)
+	// This writer will be used to print info in response to SIGINFO.
+	writer := tabwriter.NewWriter(os.Stderr, 0, 8, 0, '\t', 0)
 
 	// Read the config file.
 	var configFile = flag.String("f", localbase+"/etc/paladin.conf",
@@ -51,21 +62,37 @@ func main() {
 		g.Procs[pc.Name] = &process{Config: pc}
 	}
 
-	// Listen for a kill signal. When one comes, kill all running children
-	// before exiting.
+	// Listen for signals and respond appropriately.
 	var sigs = make(chan os.Signal)
-	signal.Notify(sigs, unix.SIGTERM, unix.SIGINT)
+	signal.Notify(sigs, unix.SIGTERM, unix.SIGINT, unix.SIGINFO)
 	go func() {
-		var sig = <-sigs
-		log.Println("Signal", sig, " received, termiating children...")
-		for i := range g.Procs {
-			if g.Procs[i].Running {
-				log.Println("killing", g.Procs[i].Config.Name)
-				unix.Kill(g.Procs[i].Status.Pid, unix.SIGTERM)
+		for true {
+			var sig = <-sigs
+			switch sig {
+			// In the case of a kill signal, terminate all
+			// children before exiting.
+			case unix.SIGTERM, unix.SIGINT:
+				log.Println("Signal", sig, " received, termiating children...")
+				for i := range g.Procs {
+					if g.Procs[i].Running {
+						log.Println("killing", g.Procs[i].Config.Name)
+						unix.Kill(g.Procs[i].Status.Pid, unix.SIGTERM)
+					}
+				}
+				log.Println("done")
+				os.Exit(0)
+			case unix.SIGINFO:
+				fmt.Println("The following children are running:")
+				fmt.Fprintln(writer, "Process\tRunning\tPath\tArgs\tUser\tGroup")
+				fmt.Fprintln(writer, "-------\t-------\t----\t----\t----\t-----")
+				for i := range g.Procs {
+					fmt.Fprintf(writer, "%s\t%s\t%s\t%s\t%s\t%s\n", g.Procs[i].Config.Name,
+						printYN(g.Procs[i].Running), g.Procs[i].Config.Path,
+						g.Procs[i].Config.Args, g.Procs[i].Config.User, g.Procs[i].Config.Group)
+					writer.Flush()
+				}
 			}
 		}
-		log.Println("done")
-		os.Exit(0)
 	}()
 
 	// Launch only leaf node processes; those that don't depend on any others.
